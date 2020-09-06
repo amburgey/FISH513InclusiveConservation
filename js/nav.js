9b778f171f727556898b5ade3d143b986e9c9054
Staci Amburgey <staci.m.amburgey@gmail.com>
2020-09-06 09:19:50 -0700

    Copied blog from Pattern Lab to local

---
+++ js/nav.js
@@ -0,0 +1,17 @@
+if (document.querySelector('.nav__btn')) {
+    var navActive = false,
+        navOpen = document.querySelector('.nav__btn'),
+        navListOpen = document.querySelector('.nav__list');
+
+    navOpen.addEventListener("click", function(event){
+    	event.preventDefault();
+
+        if (navActive === false) {
+    	    navActive = true;
+    	    navListOpen.classList.add("nav__list--is-active");
+    	} else {
+    		 navActive = false;
+    		 navListOpen.classList.remove("nav__list--is-active");
+    	}
+    });
+}
